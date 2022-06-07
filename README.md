023ca29dea340f822b2ec832144bb6881578b71b
Robert <osipovitchr@gmail.com>
2022-06-07 11:40:58 -0400

    init + begin construction on SCSS design system for application

---
+++ README.md
@@ -0,0 +1,2 @@
+## Rebuilding the ICGW Website with Next.js and MongoDB
+***Temporary static site is hosted in its place until finished rebuilding***
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Isler Custom Gun Works</title>
+        <meta name="description" content="Home Page For Isler Custom Gun Works Handguards MPX Upper-Assemblies Picatinny" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+  <h1>Hey there</h1>
+     
+    </div>
+  )
+}
