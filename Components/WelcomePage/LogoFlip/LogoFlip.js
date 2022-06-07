5d35b89ff85acdaabdf934fae1292e46d6211929
Robert <osipovitchr@gmail.com>
2022-06-07 15:02:45 -0400

    add audio files for a greater user experience

--- Components/WelcomePage/LogoFlip/LogoFlip.js
+++ Components/WelcomePage/LogoFlip/LogoFlip.js
@@ -1,12 +1,20 @@
 import React from "react";
 import BasicCrosshairs from '../../../svg/BasicCrosshairs'
+// import gunshotMp3 from '../../../audio/gunshot.mp3'
 
 import classes from "./LogoFlip.module.scss";
 
 const LogoFlip = () => {
+
+
+const play = () => {
+    var audio = document.getElementById('a1');
+    audio.play();
+}
+
   return (
   
-      <div className={classes["card-container"]}>
+      <div className={classes["card-container"]} onClick={play}>
         <div className={classes.card}>
             <div className={classes.front}>
                 <h1>I G</h1>
@@ -15,6 +23,7 @@ const LogoFlip = () => {
             </div>
             <div className={classes.back}>
                 <BasicCrosshairs />
+                <audio id='a1' src='/public.gunshot.mp3'></audio>
             </div>
         </div>
       </div>
