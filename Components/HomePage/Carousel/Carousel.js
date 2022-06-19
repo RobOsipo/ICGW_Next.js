75670a349989cf8bc4d27245490f70a3eff11fe9
Robert <osipovitchr@gmail.com>
2022-06-19 11:20:40 -0400

    finish optimized image carousel rough draft for Home Page

--- Components/HomePage/Carousel/Carousel.js
+++ Components/HomePage/Carousel/Carousel.js
@@ -1,29 +1,166 @@
 import React from 'react'
+import Image from 'next/image'
 
 import Carousel from 'react-bootstrap/Carousel';
+import classes from './Carousel.module.scss'
+
+import one from '../../../public/homeImages/gunUSA.jpg'
+import two from '../../../public/homeImages/win1.jpg'
+import three from '../../../public/homeImages/win5.jpg'
+import four from '../../../public/homeImages/gunOnDeck.jpg'
+import five from '../../../public/homeImages/gunInLight.jpg'
+import six from '../../../public/homeImages/fullR2.jpg'
+import seven from '../../../public/homeImages/FullSizeR001.jpg'
+import eight from '../../../public/homeImages/smallgun.jpg'
+import nine from '../../../public/homeImages/gunimg.jpg'
+import ten from '../../../public/homeImages/gunTip.jpg'
+import eleven from '../../../public/homeImages/gunRange.jpg'
+import twelve from '../../../public/homeImages/zombieKiller.jpg'
+import thirteen from '../../../public/homeImages/customgun.jpg'
+import fourteen from '../../../public/homeImages/partsOnTowel.jpg'
+import fifteen from '../../../public/homeImages/gun&shells.jpg'
+import sixteen from '../../../public/homeImages/workbench.jpg'
+import seventeen from '../../../public/homeImages/gunRange2.jpg'
 
 const HomeCarousel = () => {
     return (
-        <div style={{ display: 'block', width: 700, padding: 30 }}>
-          <h4>React-Bootstrap Carousel Component</h4>
-          <Carousel>
-            <Carousel.Item interval={1500}>
-              <img
+        <div id={classes.id} >
+          <Carousel variant="dark">
+            <Carousel.Item >
+              {/* <img
                 className="d-block w-100"
-    src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                src="https://i.ibb.co/2WhdH9b/gunUSA.jpg"
                 alt="Image One"
-              />
+              /> */}
+              <Image src={one} alt="Image One" />
               <Carousel.Caption>
                 <h3>Label for first slide</h3>
                 <p>Sample Text for Image One</p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <img
-                className="d-block w-100"
-    src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-                alt="Image Two"
-              />
+
+            <Carousel.Item >
+              <Image src={two} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={three} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={four} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={five} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={six} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={seven} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={eight} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={nine} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={ten} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={eleven} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={twelve} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={thirteen} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={fourteen} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={fifteen} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={sixteen} alt="Image Two" />
+              <Carousel.Caption>
+                <h3>Label for second slide</h3>
+                <p>Sample Text for Image Two</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item >
+              <Image src={seventeen} alt="Image Two" />
               <Carousel.Caption>
                 <h3>Label for second slide</h3>
                 <p>Sample Text for Image Two</p>
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import Head from "next/head";
 import Hero from "../../Components/HomePage/Hero/Hero";
 import MainNavigation from "../../Components/SharedComponents/MainNavigation/MainNavigation";
@@ -9,7 +9,7 @@ import HomeCarousel from "../../Components/HomePage/Carousel/Carousel";
 import Footer from "../../Components/SharedComponents/Footer/Footer";
 import SlideMenu from "../../Components/SharedComponents/MainNavigation/localComponents/SlideMenu/SlideMenu";
 import SplitBg from "../../Components/HomePage/SplitBg/SplitBg";
-import UpArrow from '../../svg/UpArrow'
+import UpArrow from "../../svg/UpArrow";
 
 import classes from "../../styles/Home.module.scss";
 
@@ -29,26 +29,30 @@ const HomePage = () => {
           content="Isler Custom Gun Works Home Handguards MPX Upper-Assemblies Picatinny"
         />
         <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-  crossorigin="anonymous"
-/>
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossorigin="anonymous"
+        />
       </Head>
       <main className={classes["page-container"]} id="top">
         <MainNavigation clickHandler={hamburgerClickHandler} />
-          
+
         {openHamburger && <SlideMenu />}
         <Hero />
-        
-        
-          <HomeInfo />
-          <HomeCarousel />
-          <div className={classes.top}>
 
-          <Link href="#top"><a className={classes.anchor}><span><UpArrow /></span>Back to top</a></Link>
-          </div>
-        
+        <HomeInfo />
+        <HomeCarousel />
+        <div className={classes.top}>
+          <Link href="#top">
+            <a className={classes.anchor}>
+              <span>
+                <UpArrow />
+              </span>
+              Back to top
+            </a>
+          </Link>
+        </div>
 
         <Footer />
       </main>
