4b8c2fedf2ebdbf839a5a94cbf6954ddd7586652
Robert <osipovitchr@gmail.com>
2022-06-20 09:19:36 -0400

    created the upper assemblies products mongoose schema

---
+++ models/upperAssembliesSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const {Schema, model} = mongoose
+
+const upperAssembliesSchema = new Schema({
+    title: {
+        type: string,
+        required: true
+    },
+    imageUrl: {
+        type: string,
+        required: true,
+    },
+    mainDescription: {
+        type: string,
+        required: true
+    }
+ 
+})
+
+mongoose.models = {}
+
+export default mongoose.models.Uppers || model('Upper', upperAssembliesSchema)
